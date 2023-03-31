af99cad76384259e95acc0c3cefca092a4ba15d6
jybium <asaolujtomi@gmail.com>
2023-03-31 23:27:19 +0100

    Added animation to the landing page

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
+// FOR THE SCROLL ANIMATION
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("shown");
+    } else {
+      entry.target.classList.remove("shown");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// FOR THE HAMBURGER PART
+
 let hamburger = document.querySelector(".hamburger");
 hamburger.onclick = function () {
   navbar = document.querySelector(".nav-bar");
   navbar.classList.toggle("active");
 };
 
-let display = "STOP. SHOP. REPEAT.";
-let h1 = document.querySelector("#h1");
+// FOR THE TYPING EFFECT
 
-function dis(display) {
-  for (let i = 0; i < display.length; i++) {
-    h1.innerHTML += display[i];
+let display = "STOP. SHOP. REPEAT.";
+let ded = document.getElementById("demo");
+let speed = 200;
+let i = 0;
+
+function dis() {
+  if (i < display.length) {
+    ded.innerHTML += display[i];
+    i++;
+    setTimeout(dis, speed);
   }
 }
-dis(display);
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
+dis();
+let play = "Experience The Beauty Of Clicking Away Your Needs.";
+let j = 0;
+let demo = document.getElementById("dead");
+let fast = 100;
+function news() {
+  if (j < play.length) {
+    demo.innerHTML += play[j];
+    j++;
+    setTimeout(news, fast);
+  }
+}
+news();
+
+window.addEventListener("click", function () {
+  console.log("this is the window");
 });
 
-const hiddenElements = document.querySelectorAll(".hide");
-hiddenElements.forEach((el) => observer.observe(el));
+const greet = (greeting) => (mane) => console.log(`${greeting}, ${mane}`);
+
+greet("Good morning")("Tola");
