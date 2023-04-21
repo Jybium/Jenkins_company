d5b82618f6e0f082b09725b67a61c72742917473
jybium <asaolujtomi@gmail.com>
2023-04-21 21:30:03 +0100

    improved tutorial section filter by menu keys

--- index.js
+++ index.js
@@ -43,11 +43,10 @@ let queryLink = "https://www.googleapis.com/youtube/v3/search?";
 let channelhttp = "https://www.googleapis.com/youtube/v3/channels?";
 
 
-// const onclick = function (pop){ pop.map(el => el.addEventListener('click', () => {let text = el.textContent; console.log(text); return text}))}
-// console.log(onclick(pop))
 
 const video = (search) => {
   document.querySelector('.lds-ring').style.display = 'grid'
+  console.log(search)
   const data = fetch(
     queryLink +
     new URLSearchParams({
@@ -59,6 +58,7 @@ const video = (search) => {
     })
     )
     .then((res) => {
+      console.log(res)
       return res.json();
     })
     .then((data) => {
@@ -71,22 +71,31 @@ const video = (search) => {
         videoList.insertAdjacentHTML('beforeend', eachvideo);
       });
     })
-    // .catch((err)  => {console.error(err)
-    //   const insert = `<p class="error"> Error Loading Video: ${err.msg}</p>`
-    //   errorCan.insertAdjacentHTML('afterbegin', insert)
-    // })
-    search = document.getElementById('search').value = " ";
-    
+    .catch((err)  => {console.error(err)
+      const insert = `<p class="error"> Error Loading Video: ${err.msg}</p>`
+      errorCan.insertAdjacentHTML('afterbegin', insert)
+    })
+    // search = document.getElementById('search').value = " ";
+    errorCan.remove('p')
   };
   button.addEventListener('click', (e) =>
   {
+    e.preventDefault;
     videoList.style.display = 'none'
     let search = document.getElementById('search').value;
+    console.log(search)
     button.style.backgroundColor = 'dodgerblue'
     video(search);
   })
   search.addEventListener('keypress', (e) => {
-  videoList.style.display = 'none'
+    e.preventDefault;
+    videoList.style.display = 'none'
   let search = document.getElementById('search').value;
-  if(e.key === 'Enter') {video(search)}
+  if(e.key === 'Enter') {console.log(search);video(search)}
 })
+pop.map(el => el.addEventListener('click', (e) => {
+  e.preventDefault;
+  let text = el.textContent; 
+  console.log(text); 
+  video(text)
+}))
